527209373505cce50716f8c0ce59758f444df473
alexanderyar <103504393+alexanderyar@users.noreply.github.com>
2022-08-22 13:54:31 -0600

    task-06

--- js/task-06.js
+++ js/task-06.js
@@ -2,18 +2,23 @@ const refs = {
     inputEl: document.querySelector('#validation-input'),
 
 }
-// console.dir(refs.inputEl.dataset.length)
+console.dir(refs.inputEl.dataset.length)
 
 refs.inputEl.addEventListener('blur', onBlur)
 
 function onBlur(event) {
-    if (event.currentTarget.value.length >= refs.inputEl.dataset.length) {
-refs.inputEl.classList.remove('invalid')
-        refs.inputEl.classList.add('valid') 
-        return
-    }
-    refs.inputEl.classList.add('invalid')
+    if (event.currentTarget.value.length == refs.inputEl.dataset.length) {
+        refs.inputEl.classList.add('valid')
+        refs.inputEl.classList.remove('invalid')
 
+    } else {
+        refs.inputEl.classList.add('invalid')
+        refs.inputEl.classList.remove('valid')
+    }
+    
+    console.log(event.currentTarget.value.length)
+    console.log(refs.inputEl.dataset.length)
+    console.log(event.currentTarget.value.length == refs.inputEl.dataset.length)
 }
 
 
